73652dbae1fac93dd6148b6e8352bc6e549ea319
te amo samu <you@example.com>
2022-10-11 18:56:01 -0500

    se agrego el foother y se termino la pagina index

---
+++ cursos.php
@@ -0,0 +1,12 @@
+<?php include('./partials/index_head.php') ?>
+<title>Document</title>
+</head>
+<body>
+    <div class="containerima">
+        <img src="./images/back.jpg" alt="">
+        <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #08f;"></path></svg></div>
+    
+    </div>
+<?php include('./partials/index_nabvar.php') ?>
+<?php include('./partials/countOnUs.php') ?>
+<?php include('./partials/index_foother.php') ?>
--- partials/index_foother.php
+++ partials/index_foother.php
@@ -1,3 +1,26 @@
+    <div class="container-foother"> 
+        <div class="container-foother-service">
+            <a href="cursos.php">Asesorias Aspel</a>
+            <a href="polisa.php">Poliza de mantenimiento</a> 
+            <a href="servicios.php">Factura Electronica</a> 
+            <a href="diseño.php">E-commerce</a> 
+            <a href="timbres.php">tiembres CFDI</a> 
+            <a href="cursos.php">Paqueterias Aspel</a>
+            <a href="ventas.php">Ventas de equipos</a>
+            <a href="servicios.php">Puntos de ventas</a>
+        </div>
+        <div class="container-foother-contac">
+            <div class="numero">
+                558367-8850 / 552132-1586
+            </div>
+            <div class="gmail">
+                <p>ventas@traxsistemas.com.mx</p>
+                <p>soporte@traxsistemas.com.mx</p>
+                <p>atencionaclientes@traxsistemas.com.mx</p>
+            </div>
+        </div> 
+        <p>TRAX SISTEMAS S.A de C.V I TERMINOS Y CONDICIONES I AVISO DE PRIVACIDAD </p>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
